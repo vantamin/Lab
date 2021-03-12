8e5d835b8517f7b18ce8bbbe184fc763b9ff7d76
vantamin <vantablack.min@gmail.com>
2021-03-12 15:31:57 +0900

    Game over

--- docs/js/game-breakout.js
+++ docs/js/game-breakout.js
@@ -66,8 +66,15 @@
           dx = -dx;
         }
 
-        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        if (y + dy < ballRadius) {
           dy = -dy;
+        } else if (y + dy > canvas.height - ballRadius) {
+          if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+          } else {
+            alert('GAME OVER');
+            document.location.reload();
+          }
         }
 
         if (leftPressed && paddleX > 0) {
